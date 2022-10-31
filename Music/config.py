db0b530e5e6574327fbd0e6245b2789c949dda51
Ryzennnn <110837823+Ryzennnn@users.noreply.github.com>
2022-11-01 03:07:48 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -9,40 +9,40 @@ load_dotenv()
 get_queue = {}
 
 # Client
-SESSION_NAME = getenv('BQBs9IKUI_BjKSXH1IBYUzlPEzzkkvCBctzq-DxssRpI6W3c6GyhcpWySgVRH6qetCrLB1hO8TO6gqxUB4tcxA0QiTM5X3dnCFtgIIoPIF_VqcZm4IS1mQFmj4a7TYuyGcc0lJdjknm65azxF7zzBjiwYhkrbKeB0KWfzXV-GcNeNA7BwT2v-nDZLDAPFxP7B8bIhTTnMHKFExrBs7yVxkPi6sfqRk33sch71uRhHBnKMKsomvlH8gR1kVwxbpVITNtVES_6G8nQBovin8wqkgB0H5GuODG7aZJTBxsj0-x8kW39TdSTAvvMeLeaaib2sTS7G8LD2THRbbMa1ZVtwPgPeb4-yAA')
-API_ID = int(getenv('23184709'))
-API_HASH = getenv('f99dd916ef41cf47ee094f771855468d')
+SESSION_NAME = getenv('SESSION_NAME', 'BQApymvEihSArb-elRYwkLACQ5VV7p7YtxiNnX_JGtGK3Gd-dgBDvhpx1pUQJjWAvR2SuNXeGMLBxV5NbZKmRJJud9IVhWFNu_zOjWByKDqHE4A2k9yqL7Kcz6GrWBYT7HNxtqQ651wjOaCB3YmgCMQBkODVhA9mCHCVo8M8_p-YOB15bXUNe98eOK33gwXXaE3_HIGtx9EY9M5C6Oiq4OycEcsVm3L_kVVU_n9RkkPoEfz_dXxjSPXfMZ1RISRrTBjGRO6qZUzLAOQb7zPdcWkGUEE4eVwkrK8POLzdHVXGD1d59s937K9n1RlBFSflyxRIIscg0x02jP977NFEbHAheb4-yAA')
+API_ID = int(getenv('API_ID', '23184709'))
+API_HASH = getenv('API_HASH', 'f99dd916ef41cf47ee094f771855468d')
 
 # Bot
-BOT_USERNAME = getenv("ryznmusicbot")
-BOT_TOKEN = getenv('5524682825:AAF-R88Hx_AfetXZMg1_ykuz0Ti_psBOO6k')
+BOT_USERNAME = getenv("BOT_USERNAME", "ryzenmusicbot")
+BOT_TOKEN = getenv('BOT_TOKEN', '5561364640:AAGkgktSUqzaQ_Fubfv6Xp0nzIoESQdiLBA')
 
 # Music Setting
-DURATION_LIMIT = int(getenv('DURATION_LIMIT', '3600'))
+DURATION_LIMIT = int(getenv('DURATION_LIMIT', '5600'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ . , : ; !').split())
 
-# Database Mono
-MONGO_DB_URI = getenv("mongodb+srv://zennmusic:zennmusic@cluster0.jimla2w.mongodb.net/?retryWrites=true&w=majority")
+# Database Mongo
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://anest:anest@cluster0.osfukxh.mongodb.net/?retryWrites=true&w=majority")
 
 # Sudo
-SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
-OWNER_ID = list(map(int, getenv('OWNER_ID').split()))
+SUDO_USERS = list(map(int, getenv('SUDO_USERS', '1209395963').split()))
+OWNER_ID = list(map(int, getenv('OWNER_ID', '1534983792').split()))
 
 # Log Chat
-LOG_GROUP_ID = int(getenv("-1001889413117"))
-LOG_CHANNEL_ID = int(getenv("-1001858010993"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001678126928"))
+LOG_CHANNEL_ID = int(getenv("LOG_CHANNEL_ID", "-1001844138965"))
 
 # Assistant
-ASS_ID = int(getenv("2042511048"))
+ASS_ID = int(getenv("ASS_ID", "2042511048"))
 AUTO_LEAVE = int(getenv("AUTO_LEAVE", "1500"))
 
 # Support Channel
-CHANNEL = getenv("CHANNEL", "Ryzen_Music")
-GROUP = getenv("GROUP", "Ryzen_Music")
+CHANNEL = getenv("CHANNEL", "Zarchxyz")
+GROUP = getenv("GROUP", "Exclusivedating")
 
 # Update Stream
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-REPO_URL = getenv("REPO_URL", "https://github.com/Ryzenstd/Ryzen-Music")
+REPO_URL = getenv("REPO_URL", "https://github.com/muhammadrizky16/KyyMusic")
 GIT_TOKEN = getenv(
     "GIT_TOKEN",
     b64decode("Z2hwX1kyeXl2d0MxdzBycFVkRlVMMUZjQ1hNQXd1TTFvNjIwVEZEbA==").decode(
@@ -55,7 +55,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 # Broadcast
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "True"))
 
-SUDO_USERS.append("2042511048")
+OWNER_ID.append("1209395963")
 OWNER_ID.append("1534983792")
